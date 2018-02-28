2205a7aaf58a91b7001800b8acaeb0e53ae11476
Edward Mike <ed.jmike@gmail.com>
2018-02-28 21:42:34 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -5,7 +5,7 @@ describe "current_age_for_birth_year method" do
     age_of_person = current_age_for_birth_year(1984)
 
     def current_age_for_birth_year(birth_year)
-    2003 = birth_year
+    2003 - birth_year
 
     expect(age_of_person).to eq(19)
 
